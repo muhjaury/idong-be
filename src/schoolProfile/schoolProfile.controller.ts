557e9faf4071fba5e2afbc084ddbdd79881944dd
Muhammad Jaury <muhjaury@gmail.com>
2025-03-28 20:13:19 +0700

    Add /fetchSchoolProfile & /deleteSchoolProfileAPIs and update /registerSchoolProfile API

--- src/schoolProfile/schoolProfile.controller.ts
+++ src/schoolProfile/schoolProfile.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SchoolProfileDTO } from './dto/schoolProfile.dto';
 import { SchoolProfileService } from './schoolProfile.service';
 
@@ -6,9 +6,27 @@ import { SchoolProfileService } from './schoolProfile.service';
 export class SchoolProfileController {
   constructor(private readonly profileService: SchoolProfileService) {}
 
+  @Get('fetchSchoolProfile')
+  async findAllAdmin() {
+    const init = await this.profileService.findAll();
+    if (init) {
+      return { status: 'SUCCESS', data: init };
+    }
+    return { status: 'ERROR' };
+  }
+
   @Post('registerSchoolProfile')
   async registerSchoolProfile(@Body() dto: SchoolProfileDTO) {
     const init = await this.profileService.registerSchoolProfile(dto);
+    if (init?.data) {
+      return { status: 'SUCCESS', data: init.data };
+    }
+    return { status: 'ERROR' };
+  }
+
+  @Post('deleteSchoolProfile')
+  async deleteUser(@Body() id: number) {
+    const init = await this.profileService.deleteSchoolProfile(id);
     if (init?.data) {
       return { status: 'SUCCESS', data: init.data };
     }
--- src/schoolProfile/schoolProfile.service.ts
+++ src/schoolProfile/schoolProfile.service.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { decryptData } from '../utils/dataManipulation';
 import { extactData } from '../utils/extractData';
 import { SchoolProfileDTO } from './dto/schoolProfile.dto';
 import { SchoolProfile } from './schoolProfile.entity';
@@ -9,27 +8,60 @@ import { SchoolProfile } from './schoolProfile.entity';
 export class SchoolProfileService {
   constructor(
     @Inject('USER_REPOSITORY')
-    private profileRepository: Repository<SchoolProfile>,
+    private schoolProfileRepository: Repository<SchoolProfile>,
   ) {}
+
+  findAll() {
+    return this.schoolProfileRepository.find();
+  }
 
   async registerSchoolProfile(dto: SchoolProfileDTO) {
     const data = extactData(dto);
 
-    const principleName = decryptData(data.principleName);
+    const principleName = data.principleName;
     const principleFile = data.principleFile;
-    const principleGreeting = decryptData(data.principleGreeting);
+    const principleGreeting = data.principleGreeting;
     const visionMissionFile = data.visionMissionFile;
     const orgStructureFile = data.orgStructureFile;
     const academicCalenderFile = data.academicCalenderFile;
 
-    const query =
-      'INSERT INTO school_profile ( `principleName`, `principleFile`, `principleGreeting`, `visionMissionFile`, `orgStructureFile`, `academicCalenderFile`) VALUES (' +
-      `"${principleName}","${principleFile}","${principleGreeting}","${visionMissionFile}","${orgStructureFile}","${academicCalenderFile}"` +
-      ');';
+    const selectQuery = 'SELECT * FROM school_profile';
+    const executeSelectQuery =
+      await this.schoolProfileRepository.query(selectQuery);
+    if (executeSelectQuery.length > 0) {
+      const updateQuery = `UPDATE school_profile 
+                            SET principleName="${principleName}",
+                            principleFile="${principleFile}",
+                            principleGreeting="${principleGreeting}",
+                            visionMissionFile="${visionMissionFile}",
+                            orgStructureFile="${orgStructureFile}",
+                            academicCalenderFile="${academicCalenderFile}" 
+                            WHERE id=${executeSelectQuery[0].id}`;
+      const executeUpdateQuery =
+        await this.schoolProfileRepository.query(updateQuery);
+      if (executeUpdateQuery !== undefined) {
+        return { data: principleName };
+      }
+    } else {
+      const insertQuery =
+        'INSERT INTO school_profile ( `principleName`, `principleFile`, `principleGreeting`, `visionMissionFile`, `orgStructureFile`, `academicCalenderFile`) VALUES (' +
+        `"${principleName}","${principleFile}","${principleGreeting}","${visionMissionFile}","${orgStructureFile}","${academicCalenderFile}"` +
+        ');';
 
-    const executeQuery = await this.profileRepository.query(query);
+      const executeInsertQuery =
+        await this.schoolProfileRepository.query(insertQuery);
+      if (executeInsertQuery !== undefined) {
+        return { data: principleName };
+      }
+    }
+
+    return { data: null };
+  }
+
+  async deleteSchoolProfile(id: number) {
+    const executeQuery = await this.schoolProfileRepository.delete(id);
     if (executeQuery !== undefined) {
-      return { data: principleName };
+      return { data: id };
     }
     return { data: null };
   }
